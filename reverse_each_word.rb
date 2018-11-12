b4d1b6432cfbdbc91cd8efcebe06eaeda56ec2e3
biancacharlotin <biancacharlotin@gmail.com>
2018-11-12 16:36:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word_with_each(string)
 
-  original_array = string.split(" ")
+  array = string.split(" ")
   return_array = []
-  original_array.each do|string|
+  array.each do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
@@ -10,11 +10,11 @@ end
 
 def reverse_each_word(string)
   array = string.split(" ") 
-  test_array = []
+  array2 = []
   array.collect do|string| 
-    test_array << string.reverse 
+  array2 << string.reverse 
   end
-  test_array.join(" ")
+  arrray2.join(" ")
 end
 
 
